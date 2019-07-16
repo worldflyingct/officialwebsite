bd0c0b568f8b7eee2c16fe881fa2b42d71a73feb
jevian ma(马作伟) <jevian_ma@126.com>
2019-07-16 23:59:20 +0800

    优化gettoken的命令，让格式与微信格式一样。

--- server/api/getwxtoken.php
+++ server/api/getwxtoken.php
@@ -1,16 +1,18 @@
 <?php
 $wxtokenmsg = GetWxToken ();
 $nowtime = time();
-if ($nowtime - intval($wxtokenmsg["wxtokentime"]) > 3600) {
+$expiresin = 7200 - ($nowtime - intval($wxtokenmsg["wxtokentime"]));
+if ($expiresin < 3600) {
 	$wxappid = GetWxAppId ();
 	$wxappsecret = GetWxAppSecret ();
 	$url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$wxappid."&secret=".$wxappsecret;
 	$res = file_get_contents($url);
 	$obj = json_decode($res, true);
 	$accesstoken = $obj["access_token"];
+	$expiresin = 7200;
 	SetWxToken ($accesstoken, $nowtime);
 } else {
 	$accesstoken = $wxtokenmsg["wxaccesstoken"];
 }
 
-echo json_encode(array("access_token" => $accesstoken), JSON_UNESCAPED_UNICODE);
+echo json_encode(array("access_token" => $accesstoken, "expires_in" => $expiresin), JSON_UNESCAPED_UNICODE);

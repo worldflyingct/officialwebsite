cb1bf7b2cc3a71cd9e392079bd2fe9cd515936db
jevian ma(马作伟) <jevian_ma@126.com>
2020-02-10 16:28:59 +0800

    修改数据库表引擎

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- 主机:                           localhost
--- Server version:               5.1.73 - Source distribution
--- Server OS:                    unknown-linux-gnu
--- HeidiSQL 版本:                  10.1.0.5464
+-- 服务器版本:                        5.1.73 - Source distribution
+-- 服务器操作系统:                      unknown-linux-gnu
+-- HeidiSQL 版本:                  10.3.0.5771
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,16 +10,17 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping structure for table homepage.wf_config
+-- 导出  表 homepage.wf_config 结构
 CREATE TABLE IF NOT EXISTS `wf_config` (
   `configid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `ckey` varchar(16) NOT NULL DEFAULT '',
-  `cvalue` longtext NOT NULL,
+  `ckey` varchar(32) NOT NULL DEFAULT '',
+  `cvalue` tinytext NOT NULL,
   PRIMARY KEY (`configid`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
--- Dumping structure for table homepage.wf_group
+-- 数据导出被取消选择。
+
+-- 导出  表 homepage.wf_group 结构
 CREATE TABLE IF NOT EXISTS `wf_group` (
   `groupid` bigint(20) NOT NULL AUTO_INCREMENT,
   `groupname` varchar(50) NOT NULL DEFAULT '',
@@ -29,11 +30,14 @@ CREATE TABLE IF NOT EXISTS `wf_group` (
   `getarticledesc` tinyint(1) NOT NULL DEFAULT '0',
   `getspiderlist` tinyint(1) NOT NULL DEFAULT '0',
   `pushbaidu` tinyint(1) NOT NULL DEFAULT '0',
+  `getconfig` tinyint(1) NOT NULL DEFAULT '0',
+  `setconfig` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`groupid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
--- Dumping structure for table homepage.wf_news
+-- 数据导出被取消选择。
+
+-- 导出  表 homepage.wf_news 结构
 CREATE TABLE IF NOT EXISTS `wf_news` (
   `articleid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
@@ -47,10 +51,11 @@ CREATE TABLE IF NOT EXISTS `wf_news` (
   `publishtime` datetime NOT NULL,
   `articlestatus` tinyint(1) NOT NULL,
   PRIMARY KEY (`articleid`)
-) ENGINE=MyISAM AUTO_INCREMENT=228 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=280 DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
--- Dumping structure for table homepage.wf_spiderlog
+-- 数据导出被取消选择。
+
+-- 导出  表 homepage.wf_spiderlog 结构
 CREATE TABLE IF NOT EXISTS `wf_spiderlog` (
   `spiderlogId` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL DEFAULT '',
@@ -58,10 +63,11 @@ CREATE TABLE IF NOT EXISTS `wf_spiderlog` (
   `IP` varchar(50) NOT NULL DEFAULT '',
   `time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
   PRIMARY KEY (`spiderlogId`)
-) ENGINE=MyISAM AUTO_INCREMENT=57 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=25519 DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
--- Dumping structure for table homepage.wf_user
+-- 数据导出被取消选择。
+
+-- 导出  表 homepage.wf_user 结构
 CREATE TABLE IF NOT EXISTS `wf_user` (
   `userid` bigint(20) NOT NULL AUTO_INCREMENT,
   `user` varchar(50) DEFAULT '',
@@ -72,9 +78,21 @@ CREATE TABLE IF NOT EXISTS `wf_user` (
   `lastlogin` datetime DEFAULT '1970-01-01 00:00:00',
   `userstatus` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`userid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+-- 数据导出被取消选择。
+
+-- 导出  表 homepage.wf_wxuser 结构
+CREATE TABLE IF NOT EXISTS `wf_wxuser` (
+  `wxuserid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `openid` varchar(50) NOT NULL DEFAULT '',
+  `usermsg` text NOT NULL,
+  PRIMARY KEY (`wxuserid`),
+  UNIQUE KEY `openid` (`openid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- 数据导出被取消选择。
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
